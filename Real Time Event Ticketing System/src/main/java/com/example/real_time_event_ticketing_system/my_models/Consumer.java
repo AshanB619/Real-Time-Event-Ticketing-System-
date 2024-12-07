2203b520daed748581f21e6e60efc5f2b23b4f7c
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-12-07 14:02:32 +0530

    update

---
+++ Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_models/Consumer.java
@@ -0,0 +1,45 @@
+package com.example.real_time_event_ticketing_system.my_models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Consumer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long customer_Id;
+    private String customer_Name;
+    private int customer_retrieval_Rate;
+    private int total_Ticket_By_Customer;
+
+    public Consumer() {
+
+    }
+
+    public Long getCustomer_Id() {
+        return customer_Id;
+    }
+    public void setCustomer_Id(Long customer_Id) {
+        this.customer_Id = customer_Id;
+    }
+    public String getCustomer_Name() {
+        return customer_Name;
+    }
+    public void setCustomer_Name(String customer_Name) {
+        this.customer_Name = customer_Name;
+    }
+    public int getCustomer_retrieval_Rate() {
+        return customer_retrieval_Rate;
+    }
+    public void setCustomer_retrieval_Rate(int customer_retrieval_Rate) {
+        this.customer_retrieval_Rate = customer_retrieval_Rate;
+    }
+    public int getTotal_Ticket_By_Customer() {
+        return total_Ticket_By_Customer;
+    }
+    public void setTotal_Ticket_By_Customer(int total_Ticket_By_Customer) {
+        this.total_Ticket_By_Customer = total_Ticket_By_Customer;
+
+    }
+    
+}
--- Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_models/Tickets.java
+++ Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_models/Tickets.java
@@ -8,17 +8,10 @@ public class Tickets {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long ticket_id;
-    private String status_of_ticket;
-    private int vendor_id;
-
+    private boolean status_of_ticket;
 
     public Tickets() {
 
-    }
-
-    public Tickets(String status_of_ticket, int vendor_id) {
-        this.status_of_ticket = status_of_ticket;
-        this.vendor_id = vendor_id;
     }
 
     public Long getTicket_id() {
@@ -27,19 +20,16 @@ public class Tickets {
     public void setTicket_id(Long ticket_id) {
         this.ticket_id = ticket_id;
     }
-    public String getStatus_of_ticket() {
+    public boolean isStatus_of_ticket() {
         return status_of_ticket;
-    }
-    public void setStatus_of_ticket(String status_of_ticket) {
-        this.status_of_ticket = status_of_ticket;
 
     }
-    public int getVendor_id() {
-        return vendor_id;
+    public void setStatus_of_ticket(boolean status_of_ticket) {
+        this.status_of_ticket = status_of_ticket;
     }
-    public void setVendor_id(int vendor_id) {
-        this.vendor_id = vendor_id;
-    }
+
+
+
 
 
 
