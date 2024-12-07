75ba206e4d08721910272c6287f8b8b711821f44
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-12-07 23:00:04 +0530

    update

---
+++ Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_service/Vendor_threading_service.java
@@ -0,0 +1,7 @@
+package com.example.real_time_event_ticketing_system.my_service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Vendor_threading_service {
+}
---
+++ Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_service/customer_threading_service.java
@@ -0,0 +1,4 @@
+package com.example.real_time_event_ticketing_system.my_service;
+
+public class customer_threading_service {
+}
