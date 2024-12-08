32c4cd315a18282c032a497d99fdc41be05abea5
AshanB619 <162773718+AshanB619@users.noreply.github.com>
2024-12-08 20:26:58 +0530

    update

--- Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_controller/Ticket_simulation_config.java
+++ Real Time Event Ticketing System/src/main/java/com/example/real_time_event_ticketing_system/my_controller/Ticket_simulation_config.java
@@ -1,4 +1,42 @@
 package com.example.real_time_event_ticketing_system.my_controller;
 
+import com.example.real_time_event_ticketing_system.my_models.system_details;
+import com.example.real_time_event_ticketing_system.my_repository.For_Customer_Repo;
+import com.example.real_time_event_ticketing_system.my_repository.For_Vendor_Repo;
+import com.example.real_time_event_ticketing_system.my_service.Ticket_pool_Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simulation/config")
 public class Ticket_simulation_config {
+    private final Ticket_pool_Service ticket_pool_service;
+    private final For_Vendor_Repo for_Vendor_Repo;
+    private final For_Customer_Repo for_Customer_Repo;
+    private final system_details system_details;
+
+    public Ticket_simulation_config(Ticket_pool_Service ticketPoolService, For_Vendor_Repo forVendorRepo, For_Customer_Repo forCustomerRepo,system_details systemDetails) {
+        this.ticket_pool_service = ticketPoolService;
+        this.for_Vendor_Repo = forVendorRepo;
+        this.for_Customer_Repo = forCustomerRepo;
+        this.system_details = systemDetails;
+    }
+    @PostMapping("/start/simulation")
+    public void startSimulation() {
+        new Thread(()->{
+            for (int i=1;i<=system_details.getTotal_Number_of_Tickets();i++){
+                try {
+                    ticket_pool_service.addTicket(i,"system");
+                    Thread.sleep(1000L *system_details.getTickets_Release_rate());
+                }catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+
+        
+    }
 }
+
+
